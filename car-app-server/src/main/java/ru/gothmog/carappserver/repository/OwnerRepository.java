903efbb19337469571a787dd7bf455dfacd4cf98
Дмитрий Грушецкий <d_grushetskiy@dev-city.local>
2018-11-28 18:26:35 +0300

    delete

--- car-app-server/src/main/java/ru/gothmog/carappserver/repository/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.gothmog.carappserver.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.gothmog.carappserver.model.Owner;
-
-@Repository
-public interface OwnerRepository extends CrudRepository<Owner, Long> {
-}
